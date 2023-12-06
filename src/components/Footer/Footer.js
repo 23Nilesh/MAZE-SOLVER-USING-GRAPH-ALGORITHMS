3c883c9a9e3ca8333756c828d8c9cee2c6850e22
23Nilesh <nilesh.2@iitj.ac.in>
2023-12-06 15:57:00 +0530

    updated files

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Footer.scss";
+
+const Footer = () => {
+    return ( 
+        <div className="bg-dark text-center">
+            <a href="https://www.linkedin.com/in/nilesh23gupta/" className="fa fa-linkedin"></a>
+            <a href="https://github.com/23Nilesh" className="fa fa-github"></a>
+            {/* <a href="https://medium.com/@" className="fa fa-gfg"></a> */}
+        </div>
+     );
+}
+ 
+export default Footer;
